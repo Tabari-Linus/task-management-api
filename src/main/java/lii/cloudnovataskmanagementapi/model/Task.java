94c135a7670d86074107e87572107bef1d62cb8f
Mr Lii <leetamparega@gmail.com>
2025-05-27 21:48:33 +0000

    docs: add Task model default constructor and Parametized constructor

--- src/main/java/lii/cloudnovataskmanagementapi/model/Task.java
+++ src/main/java/lii/cloudnovataskmanagementapi/model/Task.java
@@ -3,13 +3,11 @@ package lii.cloudnovataskmanagementapi.model;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class Task {
