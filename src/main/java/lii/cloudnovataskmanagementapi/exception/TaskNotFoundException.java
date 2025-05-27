76febbe7752a5c8f36adf8e96ab8aea607037e51
Mr Lii <leetamparega@gmail.com>
2025-05-27 23:23:23 +0000

    docs: create and implement a custom exception TaskNotFoundException in Exceptions

---
+++ src/main/java/lii/cloudnovataskmanagementapi/exception/TaskNotFoundException.java
@@ -0,0 +1,7 @@
+package lii.cloudnovataskmanagementapi.exception;
+
+public class TaskNotFoundException extends RuntimeException {
+    public TaskNotFoundException(Long id) {
+        super("Task with ID " + id + " not found.");
+    }
+}
