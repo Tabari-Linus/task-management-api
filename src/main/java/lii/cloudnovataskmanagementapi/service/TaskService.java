60b38c92d4209a6e57a9bf944e0f368f190fdf9f
Mr Lii <leetamparega@gmail.com>
2025-05-27 22:42:25 +0000

    docs: inject the taskRepository through the constructor

--- src/main/java/lii/cloudnovataskmanagementapi/service/TaskService.java
+++ src/main/java/lii/cloudnovataskmanagementapi/service/TaskService.java
@@ -1,4 +1,16 @@
 package lii.cloudnovataskmanagementapi.service;
 
+import lii.cloudnovataskmanagementapi.repository.TaskRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class TaskService {
+
+    private TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+
 }
