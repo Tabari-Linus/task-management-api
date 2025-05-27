0e5c7c720df0c6f7d8a31d9d9e5d62327be3e81e
Mr Lii <leetamparega@gmail.com>
2025-05-27 22:26:09 +0000

    docs: Create Enum for Task priority and task status

---
+++ src/main/java/lii/cloudnovataskmanagementapi/enums/TaskEnums.java
@@ -0,0 +1,12 @@
+package lii.cloudnovataskmanagementapi.enums;
+
+public enum TaskEnums {
+
+    public enum TaskStatus {
+        PENDING, IN_PROGRESS, COMPLETED, CANCELLED
+    }
+
+    public enum TaskPriority {
+        LOW, MEDIUM, HIGH, URGENT
+    }
+}
