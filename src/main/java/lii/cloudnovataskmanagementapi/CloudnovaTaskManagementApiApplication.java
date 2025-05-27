2e66d794c257f0c844003177939887e8464124e9
Mr Lii <leetamparega@gmail.com>
2025-05-27 20:33:54 +0000

    build: create the cloudnovataskmanagementapi project using maven and java include spring web and lombok

---
+++ src/main/java/lii/cloudnovataskmanagementapi/CloudnovaTaskManagementApiApplication.java
@@ -0,0 +1,13 @@
+package lii.cloudnovataskmanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudnovaTaskManagementApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudnovaTaskManagementApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lii/cloudnovataskmanagementapi/CloudnovaTaskManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package lii.cloudnovataskmanagementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudnovaTaskManagementApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
