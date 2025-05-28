af396797a330d053fc8363e324335c539aae7a09
Mr Lii <leetamparega@gmail.com>
2025-05-28 15:42:32 +0000

    docs: add message, status, statuscode fields

--- src/main/java/lii/cloudnovataskmanagementapi/dto/ApiErrorResponse.java
+++ src/main/java/lii/cloudnovataskmanagementapi/dto/ApiErrorResponse.java
@@ -1,4 +1,12 @@
 package lii.cloudnovataskmanagementapi.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class ApiErrorResponse {
+    private String message;
+    private String status;
+    private int statusCode;
 }
