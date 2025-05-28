ab355b9b3c7af36240a141860cf8a177046ee42e
Mr Lii <leetamparega@gmail.com>
2025-05-28 10:37:28 +0000

    docs: add TaskResponse to git

---
+++ src/main/java/lii/cloudnovataskmanagementapi/dto/TaskResponse.java
@@ -0,0 +1,20 @@
+package lii.cloudnovataskmanagementapi.dto;
+
+
+import lii.cloudnovataskmanagementapi.enums.TaskStatus;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TaskResponse {
+    private UUID id;
+    private String title;
+    private String description;
+    private TaskStatus status;
+    private LocalDateTime dueDate;
+}
