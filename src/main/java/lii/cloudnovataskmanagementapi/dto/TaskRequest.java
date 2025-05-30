a2f4dccd0f309882c042a365558dee27131ed542
Mr Lii <leetamparega@gmail.com>
2025-05-30 10:26:43 +0000

    docs: modify TaskRequest to use the built in Spring Validation Annotaing the Title and Description with NoBlank and the duedate annotated as future

--- src/main/java/lii/cloudnovataskmanagementapi/dto/TaskRequest.java
+++ src/main/java/lii/cloudnovataskmanagementapi/dto/TaskRequest.java
@@ -4,14 +4,19 @@ import lii.cloudnovataskmanagementapi.enums.TaskPriority;
 import lii.cloudnovataskmanagementapi.enums.TaskStatus;
 import lombok.Data;
 
+import jakarta.validation.constraints.*;
 import java.time.LocalDateTime;
 
 @Data
 public class TaskRequest {
+
+    @NotBlank(message = "Title is required and cannot be blank")
     private String title;
+    @NotBlank(message = "Description is required and cannot be blank")
     private String description;
     private TaskStatus status;
     private TaskPriority priority;
+    @Future(message = "Due date must be in the future")
     private LocalDateTime dueDate;
 
     public TaskRequest() {}
